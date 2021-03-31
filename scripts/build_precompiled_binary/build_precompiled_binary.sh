7fa150f5b9515a037f8fb2caf1217d01a191bd37
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-03-31 13:12:25 +0100

    Trying to switch pyinstaller to use musl, not successful yet

--- scripts/build_precompiled_binary/build_precompiled_binary.sh
+++ scripts/build_precompiled_binary/build_precompiled_binary.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
+# run from project root: scripts/build_precompiled_binary/build_precompiled_binary.sh
 
-# 1. build container (see Dockerfile)
-# 2. run from project root: scripts/build_precompiled_binary/build_precompiled_binary.sh
+set -eu
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTS_DIR="$(dirname "${CURRENT_DIR}")"
+MAKE_PRG_DIR="$(dirname "${SCRIPTS_DIR}")"
+PORTABLE_EXECUTABLE_BUILD_DIR="${MAKE_PRG_DIR}/precompiled_binary"
+
+cd "$MAKE_PRG_DIR"
+
+if [ -d "${PORTABLE_EXECUTABLE_BUILD_DIR}" ]; then
+  echo "Please remove ${PORTABLE_EXECUTABLE_BUILD_DIR} before proceeding."
+  exit 1
+fi
+
 version="0.2.0_prototype"
-sudo docker run --rm -v "$(pwd)":/make_prg make_prg_precompiled_binary_builder:0.0.1 /bin/bash -c "cd make_prg && pyinstaller \
-  --distpath precompiled_binary/dist --workpath precompiled_binary/build --noconfirm --clean --onefile \
-  --specpath precompiled_binary/spec --name make_prg_${version} --hidden-import=\"sklearn.utils._weight_vector\" \
-  make_prg/__main__.py"
+sudo docker run --rm \
+  -v "$(pwd)":/src \
+  -e PYTHONHASHSEED=42 \
+  leandroishilima/make_prg_precompiled_binary_builder:0.0.1 \
+  --noconfirm \
+  --onefile \
+  --log-level DEBUG \
+  --clean \
+  --distpath /src/precompiled_binary/dist \
+  --workpath /src/precompiled_binary/build \
+  --specpath /src/precompiled_binary/spec \
+  --name make_prg_${version} \
+  --hidden-import=\"sklearn.utils._weight_vector\" \
+  make_prg/__main__.py
