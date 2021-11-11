b97799f8bf25921775d24833eadc2b72659c72db
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-11-11 13:42:43 +0000

    Finishing From_MSA_Integration tests

--- tests/integration_tests/test_from_msa.py
+++ tests/integration_tests/test_from_msa.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from make_prg.subcommands import from_msa
 from argparse import Namespace
 from tests.test_helpers import remove_dir_if_exists, are_dir_trees_equal
+from make_prg.utils.seq_utils import SequenceCurationError
 
 
 data_dir = Path("tests/integration_tests/data")
@@ -58,47 +59,80 @@ class Test_From_MSA_Integration_Full_Builds(TestCase):
         self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/match.staggereddash",
                                             data_dir / "output/match.staggereddash"))
 
-    # def test_answers_non_nested(self):
-    #     infile = data_dir / "nonmatch.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, " 5 AAACGTGGTT 6 CCCCCCCCCC 5 ")
-    #
-    #     infile = data_dir / "nonmatch.match.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, " 5 AAACGT 6 CCCCCC 5 GGTT")
-    #
-    #     infile = data_dir / "shortmatch.nonmatch.match.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, " 5 AAACGT 6 ATTTTC 5 GGTT")
-    #
-    #
-    #     infile = data_dir / "contains_n.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, "AAACG 5 T 6 C 5 GGTT")
-    #
-    #     infile = data_dir / "contains_RYKMSW.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, "AAACG 5 T 6 C 5 GGTT")
-    #
-    #     infile = data_dir / "contains_n_and_RYKMSW.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, "AAACG 5 T 6 C 5 GGTT")
-    #
-    #     infile = data_dir / "contains_n_and_RYKMSW_no_variants.fa"
-    #     aseq = PrgBuilder(infile)
-    #     self.assertEqual(aseq.prg, "AAACGTGGTT")
-    #
-    #     # with pytest.raises(Exception):
-    #     #    aseq = AlignedSeq("test/fails.fa")
-    #
-    # def test_nested_snp_backgrounds(self):
-    #     infile = data_dir / "nested_snps_seq_backgrounds.fa"
-    #     aseq = PrgBuilder(infile, min_match_length=3)
-    #     self.assertEqual(
-    #         aseq.prg, " 5 AAAA 7 T 8 C 7 AAAAAA 6 CCCC 9 T 10 G 9 CCCCCC 5 "
-    #     )
-    #
-    # def test_nested_snps_under_del(self):
-    #     infile = data_dir / "nested_snps_deletion.fa"
-    #     aseq = PrgBuilder(infile, min_match_length=1)
-    #     self.assertEqual(aseq.prg, "A 5 AA 7 C 8 T 7 AAAA 9 T 10 G 9 AA 6 A 5 AA")
+    def test___nonmatch(self):
+        options = self.prepare_options("nonmatch")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/nonmatch",
+                                            data_dir / "output/nonmatch"))
+
+    def test___nonmatch_match(self):
+        options = self.prepare_options("nonmatch.match")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/nonmatch.match",
+                                            data_dir / "output/nonmatch.match"))
+
+    def test___nonmatch_shortmatch(self):
+        options = self.prepare_options("nonmatch.shortmatch")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/nonmatch.shortmatch",
+                                            data_dir / "output/nonmatch.shortmatch"))
+
+    def test___shortmatch_nonmatch(self):
+        options = self.prepare_options("shortmatch.nonmatch")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/shortmatch.nonmatch",
+                                            data_dir / "output/shortmatch.nonmatch"))
+
+    def test___shortmatch_nonmatch_match(self):
+        options = self.prepare_options("shortmatch.nonmatch.match")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/shortmatch.nonmatch.match",
+                                            data_dir / "output/shortmatch.nonmatch.match"))
+
+    def test___contains_n(self):
+        options = self.prepare_options("contains_n")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/contains_n",
+                                            data_dir / "output/contains_n"))
+
+    def test___contains_n_and_RYKMSW(self):
+        options = self.prepare_options("contains_n_and_RYKMSW")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/contains_n_and_RYKMSW",
+                                            data_dir / "output/contains_n_and_RYKMSW"))
+
+    def test___contains_n_no_variants(self):
+        options = self.prepare_options("contains_n_no_variants")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/contains_n_no_variants",
+                                            data_dir / "output/contains_n_no_variants"))
+
+    def test___contains_RYKMSW(self):
+        options = self.prepare_options("contains_RYKMSW")
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/contains_RYKMSW",
+                                            data_dir / "output/contains_RYKMSW"))
+
+    def test___fails___a_column_full_of_Ns(self):
+        options = self.prepare_options("fails")
+        with self.assertRaises(SequenceCurationError):
+            from_msa.run(options)
+
+    def test___fails___unexpected_char_in_MSA(self):
+        options = self.prepare_options("fails_2")
+        with self.assertRaises(SequenceCurationError):
+            from_msa.run(options)
+
+    def test___nested_snp_backgrounds(self):
+        options = self.prepare_options("nested_snps_seq_backgrounds")
+        options.min_match_length = 3
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/nested_snps_seq_backgrounds",
+                                            data_dir / "output/nested_snps_seq_backgrounds"))
+
+    def test___nested_snps_under_del(self):
+        options = self.prepare_options("nested_snps_deletion")
+        options.min_match_length = 1
+        from_msa.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output/nested_snps_deletion",
+                                            data_dir / "output/nested_snps_deletion"))
