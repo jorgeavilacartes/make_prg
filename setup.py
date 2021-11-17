0acb8d4ebea3612d73d8220cd2c9b0c9cf8f10e0
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-11-17 14:11:40 +0000

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+from pathlib import Path
+
+with Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in parse_requirements(requirements_txt)
+    ]
 
 setup(
     name="make_prg",
-    version="0.3.0",
+    version="1.0.0",
     packages=find_packages(),
     url="https://github.com/rmcolq/make_prg",
     license="MIT",
     entry_points={"console_scripts": ["make_prg = make_prg.__main__:main"]},
     test_suite="nose.collector",
     tests_require=["nose >= 1.3", "hypothesis >= 4.0", "pytest"],
-    install_requires=[
-        "biopython==1.78",
-        "numpy==1.20.0",
-        "scikit-learn==0.24.1",
-        "intervaltree==3.1.0",
-        "loguru~=0.5.3",
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
