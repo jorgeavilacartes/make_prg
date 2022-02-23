fb6ea9fbddea17df348620a2475c74d19475bfd1
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-02-23 15:43:53 +0000

    Adding --long-deletion-threshold parameter

--- tests/integration_tests/test_update.py
+++ tests/integration_tests/test_update.py
@@ -19,6 +19,7 @@ class Test_Update_Integration_Full_Builds(TestCase):
             denovo_paths=str(data_dir / test_name / 'denovo_paths.txt'),
             update_DS=update_DS,
             output_prefix=output_prefix,
+            long_deletion_threshold=1000000,
             mafft='mafft',
             log=None,
             output_type=output_type.OutputType("a"),
@@ -35,6 +36,14 @@ class Test_Update_Integration_Full_Builds(TestCase):
         update.run(options)
         self.assertTrue(are_dir_trees_equal(data_dir / "truth_output_update/match_update_simple",
                                             data_dir / "output_update/match_update_simple"))
+
+    def test___update___match_update_simple_with_long_deletion(self):
+        options = self.prepare_options(test_name="match_update_simple_with_long_deletion",
+                                       update_DS=data_dir/"truth_output/match/match.update_DS.zip")
+        options.long_deletion_threshold = 10
+        update.run(options)
+        self.assertTrue(are_dir_trees_equal(data_dir / "truth_output_update/match_update_simple_with_long_deletion",
+                                            data_dir / "output_update/match_update_simple_with_long_deletion"))
 
     def test___update___match_update_complex___several_samples_and_variants(self):
         options = self.prepare_options(test_name="match_update_complex",
@@ -103,6 +112,7 @@ class Test_Update_Integration_Full_Builds(TestCase):
             denovo_paths=str(data_dir / "match_update_simple" / 'denovo_paths.txt'),
             update_DS=data_dir / "truth_output/match/match.update_DS.zip",
             output_prefix=output_prefix,
+            long_deletion_threshold=1000000,
             mafft='mafft',
             log=None,
             output_type=output_type.OutputType("a"),
