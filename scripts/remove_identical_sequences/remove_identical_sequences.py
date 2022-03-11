904b669bfe1b2c17f0e5964fb2ebe8113eefe6af
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-03-11 10:26:00 +0000

    Now removing RC seqs

--- scripts/remove_identical_sequences/remove_identical_sequences.py
+++ scripts/remove_identical_sequences/remove_identical_sequences.py
@@ -9,10 +9,16 @@ args = parser.parse_args()
 sequences_already_output = set()
 
 with open(f"{args.fasta}.unique.fa", "w") as unique_sequences_fh, \
-     open(f"{args.fasta}.repeated.fa", "w") as repeated_sequences_fh:
+     open(f"{args.fasta}.repeated.fw.fa", "w") as repeated_sequences_fw_fh, \
+     open(f"{args.fasta}.repeated.rc.fa", "w") as repeated_sequences_rc_fh:
     for record in SeqIO.parse(args.fasta, "fasta"):
-        if record.seq not in sequences_already_output:
+        seq = record.seq
+        rc_seq = record.reverse_complement().seq
+
+        if seq in sequences_already_output:
+            SeqIO.write(record, repeated_sequences_fw_fh, "fasta")
+        elif rc_seq in sequences_already_output:
+            SeqIO.write(record, repeated_sequences_rc_fh, "fasta")
+        else:
             SeqIO.write(record, unique_sequences_fh, "fasta")
             sequences_already_output.add(record.seq)
-        else:
-            SeqIO.write(record, repeated_sequences_fh, "fasta")
